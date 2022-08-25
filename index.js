e0d5ca53a6c472be5f45726a377f5d21a7280eca
Akbar Rivaldy <akbar22rivaldy@gmail.com>
2022-08-25 21:09:54 +0700

    menghapus function updateData

--- index.js
+++ index.js
@@ -1,75 +1,71 @@
 import sqlite3 from "sqlite3";
 
-// Connect to DB
-const db = new sqlite3.Database("./database.db", sqlite3.OPEN_READWRITE, err => {
-    if(err) return console.error(err.message);
-});
-
-const createTable = (tableName, columns) => {
-    db.run(`CREATE TABLE IF NOT EXISTS ${tableName}(${columns})`, (result, err) => {
+export default function simpleSqlite(databaseName) {
+    // Connect to DB
+    const db = new sqlite3.Database(`./${databaseName}.db`, sqlite3.OPEN_READWRITE, err => {
         if(err) return console.error(err.message);
     });
-}
 
-const dropTable = (tableName) => {
-    db.run(`DROP TABLE ${tableName}`);
-}
-
-const insertData = (tableName, data) => {
-    let columns = [];
-    let values = [];
-    let params = [];
-
-    for(let prop in data) {
-        columns.push(prop);
-        values.push(data[prop]);
+    const createTable = (tableName, columns) => {
+        db.run(`CREATE TABLE IF NOT EXISTS ${tableName}(${columns})`, (result, err) => {
+            if(err) return console.error(err.message);
+        });
     }
-    
-    for(let i = 0; i < values.length; i++) {
-        params.push("?");
+
+    const dropTable = (tableName) => {
+        db.run(`DROP TABLE ${tableName}`);
     }
-    
-    db.run(`INSERT INTO ${tableName}(${columns.join(",")}) VALUES (${params.join(",")})`, values, err => {
-        if(err) return console.error(err.message);
-    });
-}
 
-const updateData = (tableName, prop, id) => {
-    db.run(`UPDATE ${tableName} SET ${prop} = ? WHERE id = ${id}`, err => {
-        if(err) return console.error(err.message)
-    });
-}
+    const insertData = (tableName, data) => {
+        let columns = [];
+        let values = [];
+        let params = [];
 
-const selectAll = (tableName, callback) => {
-    db.all(`SELECT * FROM ${tableName}`, [], (err, rows) => {
-        if(err) return console.error(err.message);
-        if(typeof callback == "function"){
-            callback(rows);
+        for(let prop in data) {
+            columns.push(prop);
+            values.push(data[prop]);
         }
-    });
-}
-
-const selectOne = (tableName, data , callback) => {
-    db.all(`SELECT * FROM ${tableName} WHERE ${data.prop}=?`, [data.value], (err, rows) => {
-        if(err) return console.error(err.message);
-        if(typeof callback == "function") {
-            callback(rows);
+        
+        for(let i = 0; i < values.length; i++) {
+            params.push("?");
         }
-    })
+        
+        db.run(`INSERT INTO ${tableName}(${columns.join(",")}) VALUES (${params.join(",")})`, values, err => {
+            if(err) return console.error(err.message);
+        });
+    }
+
+    const selectAll = (tableName, callback) => {
+        db.all(`SELECT * FROM ${tableName}`, [], (err, rows) => {
+            if(err) return console.error(err.message);
+            if(typeof callback == "function"){
+                callback(rows);
+            }
+        });
+    }
+
+    const selectOne = (tableName, data , callback) => {
+        db.all(`SELECT * FROM ${tableName} WHERE ${data.prop}=?`, [data.value], (err, rows) => {
+            if(err) return console.error(err.message);
+            if(typeof callback == "function") {
+                callback(rows);
+            }
+        })
+    }
+
+    const deleteOne = (tableName, data) => {
+        db.run(`DELETE FROM ${tableName} WHERE ${data.prop}=?`, [data.value], err => {
+            if(err) return console.error(err.message);
+        })
+    }
+
+    return {
+        createTable,
+        dropTable,
+        insertData,
+        selectAll,
+        selectOne,
+        deleteOne
+    }
 }
 
-const deleteOne = (tableName, data) => {
-    db.run(`DELETE FROM ${tableName} WHERE ${data.prop}=?`, [data.value], err => {
-        if(err) return console.error(err.message);
-    })
-}
-
-export default {
-    createTable,
-    dropTable,
-    insertData,
-    updateData,
-    selectAll,
-    selectOne,
-    deleteOne
-}
